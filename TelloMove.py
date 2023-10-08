fa35eca8c69bdb233a134f6a52eabe2ae8f9ccb9
nicolas-budin <np.budin@gmail.com>
2023-10-08 20:28:26 +0200

    added logger + exceptions

--- TelloMove.py
+++ TelloMove.py
@@ -1,12 +1,28 @@
 # Importing arcade module
+import logging
+
 import arcade
 from djitellopy import Tello
 import cv2
 
+
 # Creating MainGame class
 class MainGame(arcade.Window):
+
+
+    HANDLER = logging.StreamHandler()
+    FORMATTER = logging.Formatter('[%(levelname)s] %(filename)s - %(lineno)d - %(message)s')
+    HANDLER.setFormatter(FORMATTER)
+
+    LOGGER = logging.getLogger('tello')
+    LOGGER.addHandler(HANDLER)
+    LOGGER.setLevel(logging.INFO)
+
     def __init__(self):
         super().__init__(600, 600, title="Drone Movement")
+
+        self.isCamOn = False;
+        self.isDroneOn = False;
 
         # Initializing the initial x and y coordinated
         self.x = 250
@@ -16,35 +32,102 @@ class MainGame(arcade.Window):
         self.vel_x = 0
         self.vel_y = 0
 
+        self.vel_prev_x = 0
+        self.vel_prev_y = 0
+
         # connects to drone
         self.tello = Tello()
-        self.tello.connect()
-        print(self.tello.get_battery())
 
-        # starts cam stream
-        self.tello.streamon();
-        self.img = self.get_image()
+        try:
+            self.tello.connect()
+
+            self.LOGGER.info('Battery % {}'.format(self.tello.get_battery()))
+
+            # starts cam stream
+            try:
+                self.tello.streamon();
+                self.img = self.get_image()
+            except Exception as c:
+                self.LOGGER.error(c)
+                self.isCamOn = False
+
+        except Exception as d:
+            self.LOGGER.error(d)
+            self.isDroneOn = False;
+            self.isCamOn = False
 
     # Creating on_draw() function to draw on the screen
     def on_draw(self):
+
         arcade.start_render()
 
         # Drawing the rectangle using draw_rectangle_filled function
-        arcade.draw_circle_filled(self.x, self.y, 25, arcade.color.GREEN)
+        arcade.draw_circle_filled(self.x, self.y, 5, arcade.color.GRAY)
 
         # update img
-        cv2.imshow("Image", self.img)
+        if self.isCamOn:
+            cv2.imshow("Image", self.img)
 
     # Creating on_update function to
     # update the x coordinate
     def on_update(self, delta_time):
-        self.x += self.vel_x * delta_time
-        self.y += self.vel_y * delta_time
 
+        # display
+        self.x = 300 + self.vel_x * 10
+        self.y = 300 + self.vel_y * 10
+
+        # drone
+        if (self.vel_x != self.vel_prev_x) or (self.vel_y != self.vel_prev_y):
+
+            self.vel_prev_x = self.vel_x
+            self.vel_prev_y = self.vel_y
+
+            # drone
+            self.LOGGER.info('x : {}, y : {}'.format(self.vel_x, self.vel_y))
+            if self.isDroneOn:
+                try:
+                    self.tello.send_rc_control(self.vel_x, self.vel_y)
+                except Exception as d:
+                    self.LOGGER.error(d)
+                    self.LOGGER.warning("Drone not able to get direction command")
+                    self.isDroneOn = False;
+                    self.isCamOn = False
+            else:
+                self.LOGGER.warning("Drone not available for direction command")
+
+        # images
         self.img = self.get_image()
 
     def get_image(self):
-        return cv2.resize(self.tello.get_frame_read().frame, (360, 240))
+        return cv2.resize(self.tello.get_frame_read().frame, (360, 240)) if self.isCamOn else None
+
+    def takeOff(self):
+        if self.isDroneOn:
+            self.LOGGER.info("Drone taking off...")
+            try:
+                self.tello.takeoff()
+                self.LOGGER.info("Drone took off")
+            except Exception as d:
+                self.LOGGER.error(d)
+                self.LOGGER.warning("Drone failed to take off")
+                self.isDroneOn = False;
+                self.isCamOn = False
+        else:
+            self.LOGGER.warning("Drone not available for take off")
+
+    def land(self):
+        if self.isDroneOn:
+            self.LOGGER.info("Drone landing...")
+            try:
+                self.tello.land()
+                self.LOGGER.info("Drone landed")
+            except Exception as d:
+                self.LOGGER.error(d)
+                self.LOGGER.warning("Drone failed to land")
+                self.isDroneOn = False;
+                self.isCamOn = False
+        else:
+            self.LOGGER.warning("Drone not available for landing")
 
     # Creating function to change the velocity
     # when button is pressed
@@ -53,41 +136,21 @@ class MainGame(arcade.Window):
         # Checking the button pressed
         # and changing the value of velocity
         if symbol == arcade.key.UP:
-            self.vel_y = 300
-            print("Up arrow key is pressed")
+            self.vel_y += 10
+            self.LOGGER.info("Up arrow key is pressed")
         elif symbol == arcade.key.DOWN:
-            self.vel_y = -300
-            print("Down arrow key is pressed")
+            self.vel_y -= 10
+            self.LOGGER.info("Down arrow key is pressed")
         elif symbol == arcade.key.LEFT:
-            self.vel_x = -300
-            print("Left arrow key is pressed")
+            self.vel_x -= 10
+            self.LOGGER.info("Left arrow key is pressed")
         elif symbol == arcade.key.RIGHT:
-            self.vel_x = 300
-            print("Right arrow key is pressed")
+            self.vel_x += 10
+            self.LOGGER.info("Right arrow key is pressed")
         elif symbol == arcade.key.SPACE:
-            print("Drone taking off...")
-            self.tello.takeoff()
-            print("Drone took off")
+            self.takeOff()
         elif symbol == arcade.key.RETURN:
-            print("Drone landing...")
-            self.tello.land()
-            self.tello.land()
-            print("Drone landed")
-
-
-
-    def on_key_release(self, symbol, modifier):
-
-        # Checking the button released
-        # and changing the value of velocity
-        if symbol == arcade.key.UP:
-            self.vel_y = 0
-        elif symbol == arcade.key.DOWN:
-            self.vel_y = 0
-        elif symbol == arcade.key.LEFT:
-            self.vel_x = 0
-        elif symbol == arcade.key.RIGHT:
-            self.vel_x = 0
+            self.land()
 
 
 # Calling MainGame class
